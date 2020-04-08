d9d8681433f71ddff8439b41b20f33b7ba8abd39
muenzhang <63294124+muenzhang@users.noreply.github.com>
2020-04-08 19:29:08 +0800

    疫情模拟1.2

--- 疫情.py
+++ 疫情.py
@@ -17,10 +17,11 @@ def printlist(a):
     print()
 
 def createpeople(list,people):
-    for i in range(len(list)-1):
+    for i in range(len(list)):
         for j in range(people):
             list[i][j]=1
     list[0][0]=2
+    list[0][1]=0
     return list
 
 def listrandom(list):
@@ -33,19 +34,19 @@ def listrandom(list):
 def goto(list,x,y,a):
     if list[y][x]!=0:
         if a==1:
-            if y-1>=0 and list[y-1][x]!=1 and list[y-1][x]!=2:
+            if y-1>=0 and list[y-1][x]==0:
                 list[y-1][x]=list[y][x]
                 list[y][x]=0
         if a==2:
-            if y+1<len(list) and list[y+1][x]!=1 and list[y+1][x]!=2:
+            if y+1<len(list) and list[y+1][x]==0:
                 list[y+1][x]=list[y][x]
                 list[y][x]=0    
         if a==3:
-            if x-1>=0 and list[y][x-1]!=1 and list[y][x-1]!=2:
+            if x-1>=0 and list[y][x-1]==0:
                 list[y][x-1]=list[y][x]
                 list[y][x]=0
         if a==4:
-            if x+1<len(list[y]) and list[y][x+1]!=1 and list[y][x+1]!=2:
+            if x+1<len(list[y]) and list[y][x+1]==0:
                 list[y][x+1]=list[y][x]
                 list[y][x]=0
 
@@ -94,11 +95,15 @@ def disease(list,a,x,y,q):
                 list[y][x]=q
     return list
 
-def refresh2(list,s,q):
+def refresh2(list,s,q,z):
     for i in range(len(list)):
         for j in range(len(list[i])):
+            b=random.randint(1,100)
             if list[i][j]==1:
                 list=disease(list,s,i,j,q)
+            if list[i][j]==2:
+                if b<=z:
+                    list[i][j]=0
     return list
 
 
